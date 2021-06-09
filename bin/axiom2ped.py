284087d389251d5a001229f7fca2e8dcac70c867
saemi-matis <63106729+saemi-matis@users.noreply.github.com>
2021-06-09 22:53:54 +0000

    Update axiom2ped.py
    A file generated for the the --update-ids function in PLINK

--- bin/axiom2ped.py
+++ bin/axiom2ped.py
@@ -21,6 +21,7 @@ parser = argparse.ArgumentParser(
     -a SsaTrack_Annotation_for_sharing.csv\
     -r SsaTrack_Island_riverInfo.csv\
     -i SsaTrack_Island_fishInfo.csv\
+    -c Track_SNP_Pos.txt\
     -p Iceland\
     -o SsaTrack_Island_1-8", formatter_class=RawDescriptionHelpFormatter)
 
@@ -28,6 +29,8 @@ parser.add_argument("-g", "--genotypes", type=str, required=True,
                     help="Path to AXIOM file SsaTrack (e.g. SsaTrack_Island_1-8.calls_mod.txt")
 parser.add_argument("-a", "--annotation", type=str, required=True,
                     help="Path to annotation file for SsaTrack (csv)")
+parser.add_argument("-c", "--coordinates", type=str, required=True,
+                    help="Probes coordinates (txt)")
 parser.add_argument("-r", "--rivers", type=str, required=False,
                     help="Path to river information file (csv) (required for Icelandic Salmon)")                    
 parser.add_argument("-i", "--individuals", type=str, required=False,
@@ -43,8 +46,9 @@ if len(sys.argv)==1:
     sys.exit(1)
 args = parser.parse_args()
 
+
 if args.population == "Iceland":
-    if args.individuals != None or args.rivers != None or args.individuals != None and args.rivers != None:
+    if args.individuals != None and args.rivers != None:
         pass
     else:
         sys.exit("ERROR: Since you are converting Icelandic samples, file containing information about rivers and individuals is required")
@@ -79,9 +83,10 @@ logger.info(" ".join(sys.argv))
 
 
 def main():
-    print("#INFO: Parsing genotypes")
+    print("#INFO: Parsing genotype information")
     genotypes_dict = parse_ssatrack(args.genotypes)
     ssatrackAnnotation_dict = parse_ssatrackAnnotation(args.annotation)
+    sstrackSNPcoordinates = parseCoordinates(args.coordinates)
     if args.population == "Iceland":
         print("#INFO: Population Iceland selected: Parsing individual information")
         individualsInfo_dict = parse_individualsInfo(args.individuals)
@@ -90,7 +95,9 @@ def main():
         print("#INFO: Population Stofnfiskur selected: No individual information presnent (expected)")
         individualsInfo_dict = parseStofnfiskurIndividuals(args.genotypes)
         riversInfo_dict = {}
-    write_plinkFiles(genotypes_dict, ssatrackAnnotation_dict, riversInfo_dict, individualsInfo_dict, args.out_name, args.population)
+    updateIdsFile = writePopFileForSNPdevelopment(individualsInfo_dict, args.out_name)
+    write_plinkFiles(genotypes_dict, ssatrackAnnotation_dict, riversInfo_dict, individualsInfo_dict, args.out_name, args.population, sstrackSNPcoordinates)
+    print(f"#INFO: To update population IDs to Ice, run plink with --update-ids {updateIdsFile}")
     print(f"Script exceuted successfully, check {logFileName}.log for more details")
 
 
@@ -116,6 +123,46 @@ def parse_ssatrackAnnotation(annotation):
     return(ssatrackAnnotation_dict)
 
 
+def parseCoordinates(coordinates):
+    sstrackSNPcoordinates  ={}
+    no_positions = 0
+    with open(coordinates, "r") as f_in:
+        _ = f_in.readline()
+        for line in f_in.readlines():
+            sstrackSNPcoordinate = {}
+            position = 0
+            if "Mito" in line:
+                probe, chromosome = line.strip().split()
+                sstrackSNPcoordinate[probe] = {
+                    "probe":probe,
+                    "chromosome":"ssaMT",
+                    "position":"0"
+                }
+                no_positions += 1
+                sstrackSNPcoordinates[probe] = sstrackSNPcoordinate
+            elif "unkn" in line:
+                probe = line.strip().split()[0]
+                sstrackSNPcoordinate[probe] = {
+                    "probe":probe,
+                    "chromosome":"0",
+                    "position":"0"
+                }
+                no_positions += 1
+                sstrackSNPcoordinates[probe] = sstrackSNPcoordinate
+            else:
+                probe, chromosome, position = line.strip().split()
+                sstrackSNPcoordinate[probe] = {
+                    "probe":probe,
+                    "chromosome":chromosome,
+                    "position":str(position)
+                }
+                no_positions += 1
+                sstrackSNPcoordinates[probe] = sstrackSNPcoordinate
+    logger.info(f"#Positions form {no_positions} parsed from {coordinates}")
+    return(sstrackSNPcoordinates)
+            
+
+
 def parse_riverInformation(riversInfo):
     riversInfo_dict = {}
     with open(riversInfo, "r") as f_in:
@@ -138,6 +185,20 @@ def parse_individualsInfo(individualsInfo):
     return(individualsInfo_dict)
 
 
+def writePopFileForSNPdevelopment(individualsInfo_dict, out_name):
+    # Function to write a txt file to change population ID of individuals (--update-ids)
+    f_out_name = f"{out_name}_updateIds.txt"
+    with open(f_out_name, "w") as f_out:
+        for individual in individualsInfo_dict:
+            # print(individual, individualsInfo_dict)
+            old_pop = individualsInfo_dict[individual]["River_short"]
+            individual_id = individual
+            new_pop = "Ice"
+            newPopInfoLine = f"{old_pop}\t{individual_id}\t{new_pop}\t{individual_id}\n"
+            f_out.write(newPopInfoLine)
+    return(f_out_name)
+
+
 def parseStofnfiskurIndividuals(genotypes):
     individualsInfo_dict = {}
     individualsLine = ""
@@ -152,29 +213,25 @@ def parseStofnfiskurIndividuals(genotypes):
     return(individualsInfo_dict)
 
 
-def write_plinkFiles(genotypes_dict, ssatrackAnnotation_dict, riversInfo_dict, individualsInfo_dict, out_name, population):
+def write_plinkFiles(genotypes_dict, ssatrackAnnotation_dict, riversInfo_dict, individualsInfo_dict, out_name, population, sstrackSNPcoordinates):
     print("#INFO: Writing MAP File")
-    probe_order = write_mapFile(ssatrackAnnotation_dict, genotypes_dict, out_name)
+    probe_order = write_mapFile(ssatrackAnnotation_dict, genotypes_dict, out_name, sstrackSNPcoordinates)
     print("#INFO: Writing PED file")
     write_pedFile(genotypes_dict, ssatrackAnnotation_dict, riversInfo_dict, individualsInfo_dict, probe_order, out_name, population)
     return(probe_order)
 
 
-def write_mapFile(ssatrackAnnotation_dict, genotypes_dict, out_name):
+def write_mapFile(ssatrackAnnotation_dict, genotypes_dict, out_name, sstrackSNPcoordinates):
     f_out_name = out_name + ".map"
     x = 0
     probesOrder = []
     with open(f_out_name, "w") as f_out:
         for probe in ssatrackAnnotation_dict:
             if probe in genotypes_dict.keys():
-                probe_info = ssatrackAnnotation_dict[probe]
-                chromosome = ""
-                if ssatrackAnnotation_dict[probe]["Chromosome"] == "MT":
-                    chromosome = "MT"
-                else:
-                    chromosome = "0"
+                chromosome = sstrackSNPcoordinates[probe][probe]["chromosome"]
                 varID = probe
-                pos_cm, bp_coordinate = "0", "0"
+                pos_cm = "0"
+                bp_coordinate = sstrackSNPcoordinates[probe][probe]["position"]
                 f_out.write(f"{chromosome}\t{varID}\t{pos_cm}\t{bp_coordinate}\n")
                 x += 1
                 probesOrder.append(probe)
@@ -206,6 +263,7 @@ def generatepedIndividInformation(individual, riversInfo_dict, individualsInfo_d
         pedSex = parseSex(individualsInfo_dict[individual]["Sex"])
     elif population == "Stofnfiskur":
         population = "Stofn"
+        pedSex = "0"
     paternalID, maternalID, sex, phenotype = "0", "0", pedSex, "-9"
     pedIndividInformation = "\t".join([population, individual, paternalID, maternalID, sex, phenotype])
     return(pedIndividInformation)
@@ -228,6 +286,7 @@ def axiom2ped_genotype(axiom_genotype, genotypes_dict, probeSet, ssatrackAnnotat
     }
     ped_genotype = axiom2ped_dict[axiomGenotype]
     return(ped_genotype)
+
 
 ### NEED ALSO TO PARSE STOFNFISKUR, SHOULD IT BE IN THE SAME OR SEPERATE SCRIPT.
 ### PROBABLY BEST TO HAVE A FLAG IN ARGUMENTS, WITH EITHER ICELAND OR BOTH
